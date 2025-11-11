c2c91ba9df9c31e1702261a25bbbedbe731c77fc
1234 <a1234@1234s-MacBook-Pro.local>
2025-11-11 15:49:13 +0800

    上传SwiftPackage文件

---
+++ ZSSDK/Package.swift
@@ -0,0 +1,62 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZSSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZSSDK",
+            targets: ["ZSSDK"]),
+    ],
+    targets: [
+        // 各依赖库
+                .binaryTarget(
+                    name: "ZSSDK",
+                    url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/ZSSDK.xcframework.zip",
+                    checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                .binaryTarget(
+                    name: "ZSCoreKit",
+                    url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/ZSCoreKit.xcframework.zip",
+                    checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                .binaryTarget(
+                    name: "YKIAPStoreKit",
+                    url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/YKIAPStoreKit.xcframework.zip",
+                    checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                
+                // 主库
+                .binaryTarget(
+                    name: "Pay_zeus",
+                    url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/Pay_zeus.xcframework.zip",
+                    checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                
+                .binaryTarget(
+                        name: "AdjustSdk",
+                        url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/AdjustSdk.xcframework.zip",
+                        checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                .binaryTarget(
+                        name: "AdjustSigSdk",
+                        url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/AdjustSigSdk.xcframework.zip",
+                        checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                .binaryTarget(
+                        name: "Analytics_Adjust",
+                        url: "https://github.com/HiZeusai/SDKPackage/releases/download/2.1.1/Analytics_Adjust.xcframework.zip",
+                        checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+                ),
+                
+                // 把依赖关系声明清楚
+                .target(
+                    name: "ZSSDKWrapper",
+                    dependencies: ["ZSSDK", "ZSCoreKit", "YKIAPStoreKit","AdjustSdk","AdjustSigSdk","Analytics_Adjust", "Pay_zeus"],
+                    path: "Sources"
+                )
+    ]
+)
